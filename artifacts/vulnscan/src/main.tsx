ce33d724d764cb03b7cb6c83f74b80e9a65fa033
Faskid404 <aliufawas21@gmail.com>
2026-05-10 13:00:06 +0100

    feat: add keep-alive ping to prevent Render sleep

--- artifacts/vulnscan/src/main.tsx
+++ artifacts/vulnscan/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const KEEP_ALIVE_URL = "/scanner-api/api/health";
+const KEEP_ALIVE_INTERVAL = 10 * 60 * 1000;
+
+function startKeepAlive() {
+  fetch(KEEP_ALIVE_URL).catch(() => {});
+  setInterval(() => {
+    fetch(KEEP_ALIVE_URL).catch(() => {});
+  }, KEEP_ALIVE_INTERVAL);
+}
+
+startKeepAlive();
+
 createRoot(document.getElementById("root")!).render(<App />);
